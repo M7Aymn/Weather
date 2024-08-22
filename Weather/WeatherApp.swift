5613ed44de715b0449924f5b036aacdb0cad0fc6
M7Aymn <modyhemead@gmail.com>
2024-08-22 16:20:57 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Mohamed Ayman on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
